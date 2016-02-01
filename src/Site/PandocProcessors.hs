3ff6672e852f56592fb9ae32a945ef180136414b
Rafal Szymanski <me@rafal.io>
2016-01-31 19:43:23 -0500

    Pandoc processors

---
+++ src/Site/PandocProcessors.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Site.PandocProcessors where
+
+import           Text.Pandoc
+import           Text.Pandoc.Shared
+import           Text.Pandoc.Walk
+import           Text.Pandoc.Writers.HTML
+
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+import           Text.Blaze.Html (preEscapedToHtml, (!))
+import           Text.Blaze.Html.Renderer.String (renderHtml)
+import           Text.Highlighting.Kate (highlightAs, defaultFormatOpts)
+import           Text.Highlighting.Kate.Format.HTML (formatHtmlBlock)
+
+import           Hakyll
+
+-- Put code blocks into a figure environment
+processCodeBlocks :: Pandoc -> Pandoc
+processCodeBlocks = walk processCodeBlock
+
+processCodeBlock :: Block -> Block
+processCodeBlock b@(CodeBlock (_, classes, pairs) code) =
+  let lang     = getLang classes
+      caption  = lookup "caption" pairs
+      codeHtml = formatHtmlBlock defaultFormatOpts (highlightAs lang code)
+      captStr  = maybe "" (renderHtml . H.figcaption . H.span . H.toHtml) caption
+      composed = renderHtml $ H.figure ! A.class_ "code" $ do
+        preEscapedToHtml $ (renderHtml codeHtml) ++ captStr
+  in
+    RawBlock "html" composed
+
+processCodeBlock x = x
+
+-- Pandoc options with Math Mode (no TOC)
+pandocWriterOptions :: WriterOptions
+pandocWriterOptions = defaultHakyllWriterOptions {
+  writerHTMLMathMethod = MathJax ""
+}
+
+-- For pages where I want a TOC
+pandocWriterOptionsTOC :: WriterOptions
+pandocWriterOptionsTOC = defaultHakyllWriterOptions {
+  writerHTMLMathMethod = MathJax "",
+  writerTOCDepth = defaultTOCDepth,
+  writerTableOfContents = True,
+  writerStandalone = True,
+  writerTemplate = "<div class=\"toc\"><h3>Table of Contents</h3>\n$toc$\n</div>$body$",
+  writerNumberSections = True
+}
+  where
+    defaultTOCDepth = 4
+
+getLang [] = "text"
+getLang xs = head xs
--- src/Site/Util.hs
+++ src/Site/Util.hs
@@ -2,31 +2,20 @@
 
 module Site.Util where
 
-import           Text.Pandoc
-import           Text.Pandoc.Shared
-import           Text.Pandoc.Walk
-import           Text.Pandoc.Writers.HTML
-import Hakyll
-import qualified Data.Map as M
-import Data.Maybe
-
-import qualified Text.Blaze.Html5 as H
-import qualified Text.Blaze.Html5.Attributes as A
-import           Text.Blaze.Html (preEscapedToHtml, (!))
-import           Text.Blaze.Html.Renderer.String (renderHtml)
-import           Text.Highlighting.Kate (highlightAs, defaultFormatOpts)
-import           Text.Highlighting.Kate.Format.HTML (formatHtmlBlock)
-
-import           Data.Time.Format
-import           Data.Time.Clock
-
-import Data.List
-import Control.Applicative
-
+import Site.PandocProcessors
 import Site.Contexts
 
+import Hakyll
 
-import           System.FilePath    (takeBaseName, takeDirectory, takeFileName)
+import qualified Data.Map as M
+import Data.Maybe
+import Data.Time.Format
+import Data.Time.Clock
+import Data.List
+
+import Control.Applicative
+import System.FilePath (takeBaseName, takeDirectory, takeFileName)
+import Text.Pandoc.Options
 
 getPostBodies :: [Item String] -> Compiler String
 getPostBodies = return . concat . intersperse "<hr />" . map itemBody
@@ -49,47 +38,6 @@ preparePostString path =
       ((++) "posts/") $ case parsedTime of
           Nothing -> fn         -- parse failed, no date available, keep filename
           Just _  -> drop 11 fn -- get rid of the timestamp
-
--- Put code blocks into a figure environment
-
-processCodeBlocks :: Pandoc -> Pandoc
-processCodeBlocks = walk processCodeBlock
-
-processCodeBlock :: Block -> Block
-processCodeBlock b@(CodeBlock (_, classes, pairs) code) =
-  let lang     = getLang classes
-      caption  = lookup "caption" pairs
-      codeHtml = formatHtmlBlock defaultFormatOpts (highlightAs lang code)
-      captStr  = maybe "" (renderHtml . H.figcaption . H.span . H.toHtml) caption
-      composed = renderHtml $ H.figure ! A.class_ "code" $ do
-        preEscapedToHtml $ (renderHtml codeHtml) ++ captStr
-  in
-    RawBlock "html" composed
-
-processCodeBlock x = x
-
-getLang [] = "text"
-getLang xs = head xs
-
--- Pandoc options with Math Mode (no TOC)
-pandocWriterOptions :: WriterOptions
-pandocWriterOptions = defaultHakyllWriterOptions {
-  writerHTMLMathMethod = MathJax ""
-}
-
--- For pages where I want a TOC
-pandocWriterOptionsTOC :: WriterOptions
-pandocWriterOptionsTOC = defaultHakyllWriterOptions {
-  writerHTMLMathMethod = MathJax "",
-  writerTOCDepth = defaultTOCDepth,
-  writerTableOfContents = True,
-  writerStandalone = True,
-  writerTemplate = "<div class=\"toc\"><h3>Table of Contents</h3>\n$toc$\n</div>$body$",
-  writerNumberSections = True
-}
-  where
-    defaultTOCDepth = 4
-
 
 selectCustomPandocCompiler :: Item String -> Compiler (Item String)
 selectCustomPandocCompiler item = do
